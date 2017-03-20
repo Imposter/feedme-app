02e979ccf99ff758785f4ddf1d2740238c733d6b
Eyaz Rehman <eyaz.rehman@indigogames.ca>
2017-03-20 13:48:22 -0400

    Make sure that application conditions are met before opening app.

--- FeedMe/app/src/main/java/ca/impulsedev/feedme/StartupActivity.java
+++ FeedMe/app/src/main/java/ca/impulsedev/feedme/StartupActivity.java
@@ -1,15 +1,20 @@
 package ca.impulsedev.feedme;
 
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.location.LocationManager;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.provider.Settings;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.view.ContextThemeWrapper;
 import android.util.Log;
 
 import ca.impulsedev.feedme.api.AsyncTask;
 import ca.impulsedev.feedme.api.AsyncTaskResult;
-import ca.impulsedev.feedme.api.service.Api;
-import ca.impulsedev.feedme.api.service.ServiceCallback;
-import ca.impulsedev.feedme.api.service.models.Place;
 
 public class StartupActivity extends AppCompatActivity {
     private AsyncTask<Void> mStartupTask;
@@ -19,56 +24,149 @@ public class StartupActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_startup);
 
-        // TODO: initialize network and whatnot
-        mStartupTask = new AsyncTask<Void>() {
-            @Override
-            protected AsyncTaskResult<Void> process() {
-                try {
-                    // Dummy wait
-                    Thread.sleep(2500);
-
-                    Intent intent = new Intent(getApplicationContext(), SearchActivity.class);
-                    startActivity(intent);
-                    finish();
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-                return null;
-            }
-        };
-        //mStartupTask.execute();
-
-        Api.getNearbyFoodPlaces("sushi", 43.8046673, -79.2493688, 10000, 50,
-                new ServiceCallback<Api.SearchNearbyFoodPlacesResult>() {
-            @Override
-            protected void onBegin() {
-                Log.i("Api", "Getting nearby food places");
-            }
-
-            @Override
-            protected void onEnd(int code, Api.SearchNearbyFoodPlacesResult result) {
-                Log.i("Api", "Got " + result.nearby.length + " results");
-                for (Place place : result.nearby) {
-                    Log.i("Api", String.format("%s: %f located at %s", place.name, place.rating,
-                            place.vicinity));
-                }
-            }
-
-            @Override
-            protected void onCancelled() {
-                super.onCancelled();
-            }
-
-            @Override
-            protected void onError(Exception ex) {
-                ex.printStackTrace();
-            }
-        });
+        // Initialize network and location services
+        createStartupTask();
+        mStartupTask.execute();
     }
 
     @Override
     protected void onStop() {
         mStartupTask.cancel(true);
         super.onStop();
+    }
+
+    @Override
+    protected void onResume() {
+        if (!mStartupTask.isRunning()) {
+            createStartupTask();
+            mStartupTask.execute();
+        }
+        super.onPause();
+    }
+
+    @Override
+    protected void onPause() {
+        mStartupTask.cancel(true);
+        super.onPause();
+    }
+
+    private void createStartupTask() {
+        mStartupTask = new AsyncTask<Void>() {
+            @Override
+            protected AsyncTaskResult<Void> process() {
+                try {
+                    // Check if network is available
+                    if (!isWifiAvailable() && !isMobileDataAvailable()) {
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                // Prompt to open settings
+                                AlertDialog.Builder builder =
+                                        new AlertDialog.Builder(
+                                                new ContextThemeWrapper(StartupActivity.this,
+                                                        R.style.AppTheme));
+                                builder.setMessage(R.string.prompt_open_network_settings);
+                                builder.setPositiveButton(R.string.dialog_yes,
+                                        new DialogInterface.OnClickListener() {
+                                            @Override
+                                            public void onClick(DialogInterface dialog, int which) {
+                                                Intent intent = new Intent(
+                                                        Settings.ACTION_WIFI_SETTINGS);
+                                                startActivity(intent);
+                                                dialog.dismiss();
+                                            }
+                                        });
+                                builder.setNegativeButton(R.string.dialog_no,
+                                        new DialogInterface.OnClickListener() {
+                                            @Override
+                                            public void onClick(DialogInterface dialog, int which) {
+                                                finish();
+                                            }
+                                        });
+                                builder.show();
+                            }
+                        });
+                    } else if (!isGpsEnabled() && !isMobileLocationEnabled()) {
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                // Prompt to open settings
+                                AlertDialog.Builder alert =
+                                        new AlertDialog.Builder(
+                                                new ContextThemeWrapper(StartupActivity.this,
+                                                        R.style.AppTheme));
+                                alert.setMessage(R.string.prompt_open_location_settings);
+                                alert.setPositiveButton(R.string.dialog_yes,
+                                        new DialogInterface.OnClickListener() {
+                                            @Override
+                                            public void onClick(DialogInterface dialog, int which) {
+                                                Intent intent = new Intent(
+                                                        Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                                                startActivity(intent);
+                                                dialog.dismiss();
+                                            }
+                                        });
+                                alert.setNegativeButton(R.string.dialog_no,
+                                        new DialogInterface.OnClickListener() {
+                                            @Override
+                                            public void onClick(DialogInterface dialog, int which) {
+                                                finish();
+                                            }
+                                        });
+                                alert.show();
+                            }
+                        });
+                    } else {
+                        // Wait
+                        Thread.sleep(1000);
+
+                        Intent intent = new Intent(getApplicationContext(), SearchActivity.class);
+                        startActivity(intent);
+                        finish();
+                    }
+                } catch (InterruptedException ex) {
+                    ex.printStackTrace();
+                }
+                return null;
+            }
+        };
+    }
+
+    private boolean isWifiAvailable() {
+        ConnectivityManager connectivityManager
+                = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = connectivityManager.getActiveNetworkInfo();
+        return networkInfo != null && networkInfo.isConnected()
+                && networkInfo.getType() == ConnectivityManager.TYPE_WIFI;
+    }
+
+    private boolean isMobileDataAvailable() {
+        ConnectivityManager connectivityManager
+                = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = connectivityManager.getActiveNetworkInfo();
+        return networkInfo != null && networkInfo.isConnected()
+                && networkInfo.getType() == ConnectivityManager.TYPE_MOBILE;
+    }
+
+    private boolean isGpsEnabled() {
+        LocationManager locationManager =
+                (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        try {
+            locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
+            return true;
+        } catch (Exception ex) {
+            return false;
+        }
+    }
+
+    private boolean isMobileLocationEnabled() {
+        LocationManager locationManager =
+                (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        try {
+            locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER);
+            return true;
+        } catch (Exception ex) {
+            return false;
+        }
     }
 }
--- FeedMe/app/src/main/java/ca/impulsedev/feedme/api/AsyncTask.java
+++ FeedMe/app/src/main/java/ca/impulsedev/feedme/api/AsyncTask.java
@@ -1,21 +1,26 @@
 package ca.impulsedev.feedme.api;
 
 public abstract class AsyncTask<TResult> extends android.os.AsyncTask<Void, Void, AsyncTaskResult<TResult>> {
+    private boolean mRunning;
+
     @Override
     protected AsyncTaskResult<TResult> doInBackground(Void... params) {
         return process();
     }
     @Override
     protected void onPreExecute() {
+        mRunning = true;
         onBegin();
     }
     @Override
     protected void onPostExecute(AsyncTaskResult<TResult> result) {
         onEnd(result);
+        mRunning = false;
     }
     @Override
     protected void onCancelled(AsyncTaskResult<TResult> result) {
         onCancelled();
+        mRunning = false;
     }
 
     protected void onBegin() {
@@ -26,6 +31,10 @@ public abstract class AsyncTask<TResult> extends android.os.AsyncTask<Void, Void
 
     @Override
     protected void onCancelled() {
+    }
+
+    public boolean isRunning() {
+        return mRunning;
     }
 
     protected abstract AsyncTaskResult<TResult> process();
